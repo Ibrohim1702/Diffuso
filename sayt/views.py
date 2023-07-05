1deeea29378c354c94ca56708e34d114f629d36e
Ibrohim1702 <88259165+Ibrohim1702@users.noreply.github.com>
2023-07-05 13:07:02 +0500

    Urls, Views, Settings
    Urls, Views, Settings

--- sayt/views.py
+++ sayt/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(requests):
+    ctx = {
+
+    }
+    return render(requests, "index.html", ctx)
+
+
+def about(requests):
+    ctx = {
+
+    }
+    return render(requests, "about.html", ctx)
+
+
+def contact(requests):
+    ctx = {
+
+    }
+    return render(requests, "contact.html", ctx)
+
+
+def portfolio(requests):
+    ctx = {
+
+    }
+    return render(requests, "portfolio.html", ctx)
+
+
+def service(requests):
+    ctx = {
+
+    }
+    return render(requests, "service.html", ctx)
